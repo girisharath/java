df51d5a0efc5868ae1998b998315b52a5adfdebd
girisharath <64161105+girisharath@users.noreply.github.com>
2022-09-17 20:04:10 +0530

    Add files via upload

---
+++ ConcatinateValue.java
@@ -0,0 +1,19 @@
+package test;
+
+public class ConcatinateValue {
+
+	public static void main(String[] args) {
+
+		System.out.println("value is " + 20 + 20);
+
+		System.out.println("value is " + 20);
+
+		System.out.println("value is " + (20 + 20));
+
+		System.out.println("value is " + (20 - 2 * 1 - 10));
+		
+		
+		System.out.println("uwherkgkrtjkly "+56);
+	}
+
+}
